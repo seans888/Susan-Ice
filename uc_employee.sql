7b04165fb5338c5dedb4d19416378ffff2d9e4c3
leonardpotian12 <leonardpotian@yahoo.com>
2017-04-12 23:40:10 +0800

    uc_employee.sql

---
+++ uc_employee.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 12, 2017 at 05:08 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `203`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uc_employee`
+--
+
+CREATE TABLE `uc_employee` (
+  `emp_id` int(11) NOT NULL,
+  `Id_address` varchar(45) NOT NULL,
+  `emp_contact` varchar(45) NOT NULL,
+  `emp_fname` varchar(45) NOT NULL,
+  `emp_lname` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `uc_employee`
+--
+ALTER TABLE `uc_employee`
+  ADD PRIMARY KEY (`emp_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `uc_employee`
+--
+ALTER TABLE `uc_employee`
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
