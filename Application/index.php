64dd729282b70df4fc421438577adaf5269af46f
leonardpotian12 <leonardpotian@yahoo.com>
2017-07-12 08:54:05 +0800

    login.php and index.php

--- Application/index.php
+++ Application/index.php
@@ -1,15 +1,182 @@
-<?php
-/*
-UserCake Version: 2.0.2
-http://usercake.com
-*/
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+<meta name="viewport" content="width=device-width" />
+<title>Susan Ice Store </title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/jqueryslidemenu.css" type="text/css" media="screen" />
 
-require_once("models/config.php");
-if (!securePage($_SERVER['PHP_SELF'])){die();}
-if(!isUserLoggedIn()) { header("Location: login.php"); die(); }
-include("subpage/header.php");
-include("subpage/dashboard.php");
-include("subpage/footer.php");
+<!-- supersized -->
+
+<!-- supersized -->
+
+	<link rel="stylesheet" href="css/carouFredSel.css" type="text/css" media="screen" />
+<link rel="stylesheet" type="text/css" href="css/quicksand.css" />
+<link rel="stylesheet" type="text/css" href="js/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/jqueryslidemenu.js"></script>
+<script type="text/javascript" src="js/jquery.easing.min.js"></script>
+
+<!-- supersized -->
+<script type="text/javascript" src="js/supersized.3.2.7.js"></script>
+<script type="text/javascript" src="js/supersized.shutter.js"></script>
+<!-- supersized -->
+
+<!-- fancybox -->
+<script type="text/javascript" src="js/fancybox/jquery.fancybox-1.3.4.js"></script>
+<script type="text/javascript" src="js/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+<!-- fancybox -->
+
+<!-- quicksand -->
+<script type="text/javascript" src="js/portfolio_sortable.js"></script>
+<script type="text/javascript" src="js/quicksand.js"></script>
+<!-- quicksand -->
+
+<script type="text/javascript" src="js/jquery.carouFredSel-6.0.6.js"></script>
+
+<script type="text/javascript" src="js/contact.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
 
 
-?>
+<link href="http://fonts.googleapis.com/css?family=Oswald:400,300,600,700,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="page_wrapper">
+
+<div id="bgOverlay"><a  href="#" class="page_close"><img src="images/close_button.png" alt="img" /></a></div>
+
+
+<!--Supersized Controls Start-->
+
+<div id="slidecaption_wrapper">
+    <div id="slidecaption"></div>
+</div>
+<!--Supersized Controls End-->
+
+
+<!--leftSide start --> 
+<div id="leftSide">
+    <div id="logo">
+        
+            <a href="index.php">
+                <img src="images/susan logo.png" alt="logo" width="215" height="120" />
+            </a>
+       
+    </div>
+	<ul>
+			<li style="border-bottom: 1px solid rgba(127, 127, 127, 0.3); position: relative;"><a href="store/generatetrans.php" style="color: #211C1B; display: block; font-family: 'Oswald',Arial; font-weight: 600; padding: 10px 25px; text-decoration: none; text-transform: uppercase; font-size:15px;">products</a></li>
+		</ul>
+    <nav id="sidebarmenu">
+        <ul id="sidebarmenu1" class="menu">
+            <li><a href="#page_about">About</a></li>
+			<li><a href="#adminlogin">Login as Admin</a></li>
+			<li><a href="#page_contact">Feedback</a></li>
+        </ul>
+		
+    </nav>
+    
+    <div class="social_media_wrapper"> 
+      <div class="social_media"> 
+            <ul>
+                <li></li>
+                
+            </ul>
+        </div>
+       <li><img src="images/contact_us.jpg" alt="img" width="215" height="120"></li>
+    </div>
+</div>
+<!--leftside end --> 
+
+
+<!--Content Start-->
+<div class="contWrapper">
+<article id="content">
+
+
+
+<ul id="ulcontent">
+
+<!--About Us Start-->
+<li id="page_about">
+    <div class="title-wrapper">
+        <h2><center>ABOUT US </center></h2>
+    </div>
+	<center><img src="images/pasay area.png"> </center>
+    <p><center> <h2> Manlunas Street Villamor Air Base Pasay City </center></h2></p>
+
+<!--Contact Start-->
+<li id="page_contact">
+    
+    <div class="clear"></div>
+    <div class="v_space"></div>
+    
+    <div class="title-wrapper">
+        <h2>Feedback Form</h2>
+    </div>
+    <div class="contact_form">
+        <form method="post" action="sendEmail.php" name="contact-form" id="contact-form">	
+		<div id="response"></div>
+        <div id="main">
+            <div class="one_third">
+                <label>Name:</label>
+                <p><input type="text" name="name" id="name" size="30" /></p>
+            </div>
+            <div class="one_third">
+                <label>Email:</label>
+                <p><input type="text" name="email" id="email" size="30" /></p>
+            </div>
+            <div class="one_third_last">
+                <label>Subject:</label>
+                <p><input type="text" name="subject" id="subject" size="30" /></p>
+            </div>
+            <label>Message:</label>
+            <p><textarea name="message" id="message" cols="30" rows="10"></textarea></p>
+            <p><input  class="contact_button button" type="submit" name="submit" id="submit" value="Send" /></p>
+        </div>
+        </form>
+    </div> 
+</li>
+<!--Contact End-->
+
+<!--adminlogin Start-->
+<li id="adminlogin">
+   
+    <div class="title-wrapper">
+        <h2>Admin Login Form</h2>
+    </div>
+    <div class="contact_form">
+        <form method="post" action="login.php" name="contact-form" id="contact-form">	
+        <div id="main">
+            <div class="one_third">
+                <label>Username:</label>
+                <p><input type="text" name="user" id="name" size="30" /></p>
+            </div>
+            <div class="one_third">
+                <label>Password:</label>
+                <p><input type="password" name="password" id="email" size="30" /></p>
+            </div>
+            <div class="one_third_last">
+			<label>&nbsp;</label>
+                <input  class="contact_button button" type="submit" name="submit" id="submit" value="Login" />
+            </div>
+        </div>
+        </form>
+    </div> 
+</li>
+<!--adminlogin End-->
+                   
+</ul>
+</article>
+</div>
+<!--Content End-->
+
+</div>
+<!--Page wrapper End-->
+</body>
+</html>
--- Application/login.php
+++ Application/login.php
@@ -1,158 +1,48 @@
-<body style="background-color:#38ACEC;">
-
-<style>
-	input[type=text], select {
-    width: 500px;
-    padding: 12px 20px;
-    margin: 0px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-}
-input[type=password], select {
-    width: 500px;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-}
-
-input[type=submit] {
-    width: 100px;
-    background-color: #38ACEC;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-}
-
-input[type=submit]:hover {
-    background-color: #45a049;
-}
-
-div {
-	width:0px;
-	height:10px;
-    border-radius: 5px;
-    background-color: #f2f2f2;
-    padding: 20px;
-}
-</style>
-
 <?php
-/*
-UserCake Version: 2.0.2
-http://usercake.com
-*/
-
-require_once("models/config.php");
-if (!securePage($_SERVER['PHP_SELF'])){die();}
-
-//Prevent the user visiting the logged in page if he/she is already logged in
-if(isUserLoggedIn()) { header("Location: account.php"); die(); }
-
-//Forms posted
-if(!empty($_POST))
-{
-
-	$errors = array();
-	$username = sanitize(trim($_POST["username"]));
-	$password = trim($_POST["password"]);
+	//Start session
+	session_start();
 	
-	//Perform some validation
-	//Feel free to edit / change as required
-	if($username == "")
-	{
-		$errors[] = lang("ACCOUNT_SPECIFY_USERNAME");
-	}
-	if($password == "")
-	{
-		$errors[] = lang("ACCOUNT_SPECIFY_PASSWORD");
-	}
 
-	if(count($errors) == 0)
-	{
-		//A security note here, never tell the user which credential was incorrect
-		if(!usernameExists($username))
-		{
-			$errors[] = lang("ACCOUNT_USER_OR_PASS_INVALID");
-		}
-		else
-		{
-			$userdetails = fetchUserDetails($username);
-			//See if the user's account is activated
-			if($userdetails["active"]==0)
-			{
-				$errors[] = lang("ACCOUNT_INACTIVE");
-			}
-			else
-			{
-				//Hash the password and use the salt from the database to compare the password.
-				$entered_pass = generateHash($password,$userdetails["password"]);
-				
-				if($entered_pass != $userdetails["password"])
-				{
-					//Again, we know the password is at fault here, but lets not give away the combination incase of someone bruteforcing
-					$errors[] = lang("ACCOUNT_USER_OR_PASS_INVALID");
-				}
-				else
-				{
-					//Passwords match! we're good to go'
-					
-					//Construct a new logged in user object
-					//Transfer some db data to the session object
-					$loggedInUser = new loggedInUser();
-					$loggedInUser->email = $userdetails["email"];
-					$loggedInUser->user_id = $userdetails["id"];
-					$loggedInUser->hash_pw = $userdetails["password"];
-					$loggedInUser->title = $userdetails["title"];
-					$loggedInUser->displayname = $userdetails["display_name"];
-					$loggedInUser->username = $userdetails["user_name"];
-
-					//Update last sign in
-					$loggedInUser->updateLastSignIn();
-					$_SESSION["userCakeUser"] = $loggedInUser;
-					
-					//Redirect to user account page
-					header("Location: account.php");
-					die();
-				}
-			}
-		}
-	}
-}
-
-include("subpage/header.php");
-
-echo resultBlock($errors,$successes);
+	require "store/connect.php";
 	
-echo "
 
-		<div class='regbox'>
-			<form name='login' action='".$_SERVER['PHP_SELF']."' method='post'>
-			<p>
-			<label>Email:</label>
-			<input type='text' name='username' />
-			</p>
-			<p>
-			<label>Password:</label>
-			<input type='password' name='password' />
-			</p>
-			<p>
-			<label>&nbsp;</label>
-			<input type='submit' value='Login' class='submit' />
-			</p>
-			</form>
-			</div>
-		<div id='bottom'></div>";
+	function clean($str) {
+		$str = @trim($str);
+		if(get_magic_quotes_gpc()) {
+			$str = stripslashes($str);
+		}
+		return mysql_real_escape_string($str);
+	}
+	
 
+	$login = clean($_POST['user']);
+	$password = clean($_POST['password']);
+	
+	//Create query
+	$qry="SELECT * FROM user WHERE username='$login' AND password='$password'";
+	$result=mysql_query($qry);
+	//while($row = mysql_fetch_array($result))
+//  {
+//  $level=$row['position'];
+//  }
 
-include("subpage/footer.php");
+	if($result) {
+		if(mysql_num_rows($result) > 0) {
+			//Login Successful
+			session_regenerate_id();
+			$member = mysql_fetch_assoc($result);
+			$_SESSION['SESS_MEMBER_ID'] = $member['user_id'];
+			$_SESSION['SESS_FIRST_NAME'] = $member['position'];
+			session_write_close();
+			//if ($level="admin"){
+			header("location: admin/index.php");
+			exit();
+		}else {
+			//Login failed
+			header("location: index.php");
+			exit();
+		}
+	}else {
+		die("Query failed");
+	}
 ?>
-</center>
-</body>
