c88121bdfc48fdf0234b4e0b960c1f170160952e
nelainevaldez <nelainekriscelvaldez@yahoo.com>
2017-08-31 14:19:54 +0800

    PHP CODES

--- Application/index.php
+++ Application/index.php
@@ -1,182 +1,168 @@
-<!DOCTYPE html>
-<html dir="ltr" lang="en-US">
+<?php
+	require_once('../auth.php');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="UTF-8" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
-<meta name="viewport" content="width=device-width" />
-<title>Susan Ice Store </title>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="css/jqueryslidemenu.css" type="text/css" media="screen" />
-
-<!-- supersized -->
-
-<!-- supersized -->
-
-	<link rel="stylesheet" href="css/carouFredSel.css" type="text/css" media="screen" />
-<link rel="stylesheet" type="text/css" href="css/quicksand.css" />
-<link rel="stylesheet" type="text/css" href="js/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
-<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/jqueryslidemenu.js"></script>
-<script type="text/javascript" src="js/jquery.easing.min.js"></script>
-
-<!-- supersized -->
-<script type="text/javascript" src="js/supersized.3.2.7.js"></script>
-<script type="text/javascript" src="js/supersized.shutter.js"></script>
-<!-- supersized -->
-
-<!-- fancybox -->
-<script type="text/javascript" src="js/fancybox/jquery.fancybox-1.3.4.js"></script>
-<script type="text/javascript" src="js/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
-<!-- fancybox -->
-
-<!-- quicksand -->
-<script type="text/javascript" src="js/portfolio_sortable.js"></script>
-<script type="text/javascript" src="js/quicksand.js"></script>
-<!-- quicksand -->
-
-<script type="text/javascript" src="js/jquery.carouFredSel-6.0.6.js"></script>
-
-<script type="text/javascript" src="js/contact.js"></script>
-<script type="text/javascript" src="js/custom.js"></script>
-
-
-<link href="http://fonts.googleapis.com/css?family=Oswald:400,300,600,700,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+<title>Dashboard | Modern Admin</title>
+<link rel="stylesheet" type="text/css" href="css/960.css" />
+<link rel="stylesheet" type="text/css" href="css/reset.css" />
+<link rel="stylesheet" type="text/css" href="css/text.css" />
+<link rel="stylesheet" type="text/css" href="css/blue.css" />
+<link type="text/css" href="css/smoothness/ui.css" rel="stylesheet" />  
+<link rel="stylesheet" href="febe/style.css" type="text/css" media="screen" charset="utf-8">
+<script src="argiepolicarpio.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/application.js" type="text/javascript" charset="utf-8"></script>	
+<!--sa poip up-->
+<link href="src/facebox.css" media="screen" rel="stylesheet" type="text/css" />
+   <script src="lib/jquery.js" type="text/javascript"></script>
+  <script src="src/facebox.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    jQuery(document).ready(function($) {
+      $('a[rel*=facebox]').facebox({
+        loadingImage : 'src/loading.gif',
+        closeImage   : 'src/closelabel.png'
+      })
+    })
+  </script>
 </head>
+
 <body>
-<div id="page_wrapper">
-
-<div id="bgOverlay"><a  href="#" class="page_close"><img src="images/close_button.png" alt="img" /></a></div>
-
-
-<!--Supersized Controls Start-->
-
-<div id="slidecaption_wrapper">
-    <div id="slidecaption"></div>
+<!-- WRAPPER START -->
+<div class="container_16" id="wrapper">	
+<!-- HIDDEN COLOR CHANGER -->      
+      <div style="position:relative;">
+  	<!--LOGO-->
+	<div class="grid_8" id="logo">Susan Ice Store</div>
+    <div class="grid_8">
+<!-- USER TOOLS START -->
+      <div id="user_tools"><span><a href="../index.php">Logout</a></span></div>
+    </div>
+<!-- USER TOOLS END -->    
+<div class="grid_16" id="header">
+<!-- MENU START -->
+<div id="menu">
+	<ul class="group" id="menu_group_main">
+		<li class="item first" id="one"><a href="index.php" class="main current"><span class="outer"><span class="inner dashboard">Dashboard</span></span></a></li>
+		<li class="item middle" id="four"><a href="message.php" class="main"><span class="outer"><span class="inner media_library">Messages</span></span></a></li>  
+		<li class="item last" id="eight"><a href="products.php" class="main"><span class="outer"><span class="inner settings">Products</span></span></a></li>        
+    </ul>
 </div>
-<!--Supersized Controls End-->
-
-
-<!--leftSide start --> 
-<div id="leftSide">
-    <div id="logo">
-        
-            <a href="index.php">
-                <img src="images/susan logo.png" alt="logo" width="215" height="120" />
-            </a>
-       
-    </div>
-	<ul>
-			<li style="border-bottom: 1px solid rgba(127, 127, 127, 0.3); position: relative;"><a href="store/generatetrans.php" style="color: #211C1B; display: block; font-family: 'Oswald',Arial; font-weight: 600; padding: 10px 25px; text-decoration: none; text-transform: uppercase; font-size:15px;">products</a></li>
-		</ul>
-    <nav id="sidebarmenu">
-        <ul id="sidebarmenu1" class="menu">
-            <li><a href="#page_about">About</a></li>
-			<li><a href="#adminlogin">Login as Admin</a></li>
-			<li><a href="#page_contact">Feedback</a></li>
-        </ul>
-		
-    </nav>
-    
-    <div class="social_media_wrapper"> 
-      <div class="social_media"> 
-            <ul>
-                <li></li>
-                
-            </ul>
-        </div>
-       <li><img src="images/contact_us.jpg" alt="img" width="215" height="120"></li>
-    </div>
+<!-- MENU END -->
 </div>
-<!--leftside end --> 
 
 
-<!--Content Start-->
-<div class="contWrapper">
-<article id="content">
-
-
-
-<ul id="ulcontent">
-
-<!--About Us Start-->
-<li id="page_about">
-    <div class="title-wrapper">
-        <h2><center>ABOUT US </center></h2>
+<!-- CONTENT START -->
+    <div class="grid_16" id="content">
+    <!--  TITLE START  --> 
+    <div class="grid_9">
+    <h1 class="dashboard">Dashboard</h1>
     </div>
-	<center><img src="images/pasay area.png"> </center>
-    <p><center> <h2> Manlunas Street Villamor Air Base Pasay City </center></h2></p>
-
-<!--Contact Start-->
-<li id="page_contact">
+    <div class="clear">
+    </div>
+    <!--  TITLE END  -->    
+    <!-- #PORTLETS START -->
+    <div id="portlets">
     
+	<!--  SECOND SORTABLE COLUMN END -->
     <div class="clear"></div>
-    <div class="v_space"></div>
-    
-    <div class="title-wrapper">
-        <h2>Feedback Form</h2>
-    </div>
-    <div class="contact_form">
-        <form method="post" action="sendEmail.php" name="contact-form" id="contact-form">	
-		<div id="response"></div>
-        <div id="main">
-            <div class="one_third">
-                <label>Name:</label>
-                <p><input type="text" name="name" id="name" size="30" /></p>
-            </div>
-            <div class="one_third">
-                <label>Email:</label>
-                <p><input type="text" name="email" id="email" size="30" /></p>
-            </div>
-            <div class="one_third_last">
-                <label>Subject:</label>
-                <p><input type="text" name="subject" id="subject" size="30" /></p>
-            </div>
-            <label>Message:</label>
-            <p><textarea name="message" id="message" cols="30" rows="10"></textarea></p>
-            <p><input  class="contact_button button" type="submit" name="submit" id="submit" value="Send" /></p>
-        </div>
-        </form>
-    </div> 
-</li>
-<!--Contact End-->
+    <!--THIS IS A WIDE PORTLET-->
+		<div class="portlet">
+			<div class="portlet-header fixed"><img src="images/icons/user.gif" width="16" height="16" alt="Latest Registered Users" /> 
+			<label for="filter">Search</label> <input type="text" name="filter" value="" id="filter" />
+			&nbsp;&nbsp;Legend:  <img src="cancel.png">Pending &nbsp;<img src="active.png">Delivered
+			</div>
+			<div class="portlet-content nopadding">
+			<form action="" method="post">
+			
+			<table cellpadding="1" cellspacing="1" id="resultTable">
+				<thead>
+					<tr>
+						<th  style="border-left: 1px solid #C1DAD7"> Confirmation Number </th>
+						<th> Name </th>
+						<th> Address </th>
+						<th> Contact </th>
+						<th> Email </th>
+						<th> Delivery Address </th>
+						<th> Status </th>
+						<th> Delivery Type</th>
+						<th> Payment Method</th>
+						<th> Action </th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php
+					include('../store/connect.php');
+					$result = mysql_query("SELECT * FROM reservation ORDER BY firstname ASC");
+					while($row = mysql_fetch_array($result))
+						{
+							echo '<tr class="record" id="'.$row['status'].'">';
+							echo '<td style="border-left: 1px solid #C1DAD7;">'.$row['confirmation'].'</td>';
+							echo '<td>'.$row['firstname'].' '.$row['lastname'].'</td>';
+							echo '<td><div align="left">'.$row['address'].'</div></td>';
+							echo '<td><div align="left">'.$row['contact'].'</div></td>';
+							echo '<td><div align="left">'.$row['email'].'</div></td>';
+							echo '<td><div align="left">'.$row['delivery'].'</div></td>';
+							echo '<td><div align="left">'.$row['status'].'</div></td>';
+							echo '<td><div align="left">'.$row['delivery_type'].'</div></td>';
+							echo '<td><div align="left">'.$row['payment'].'</div></td>';
+							echo '<td><div align="center"><a rel="facebox" href="vieworders.php?id='.$row['confirmation'].'" title="Click To View Orders">View Orders</a> | <a rel="facebox" href="viewreport.php?id='.$row['confirmation'].'" title="Click To View Orders">Print</a> | <a rel="facebox" href="editstatus.php?id='.$row['reservation_id'].'">edit</a> | <a href="#" id="'.$row['reservation_id'].'" class="delbutton" title="Click To Delete">delete</a></div></td>';
+							echo '</tr>';
+						}
+					?> 
+				</tbody>
+			</table>
+			</form>
+			</div>
+		</div>
+<!--  END #PORTLETS -->  
+   </div>
+    <div class="clear"> </div>
+<!-- END CONTENT-->    
+  </div>
+<div class="clear"> </div>
 
-<!--adminlogin Start-->
-<li id="adminlogin">
+</div>
+</div>
+<!-- WRAPPER END -->
+
+<script src="js/jquery.js"></script>
+  <script type="text/javascript">
+$(function() {
+
+
+$(".delbutton").click(function(){
+
+//Save the link in a variable called element
+var element = $(this);
+
+//Find the id of the link that was clicked
+var del_id = element.attr("id");
+
+//Built a url to send
+var info = 'id=' + del_id;
+ if(confirm("Sure you want to delete this update? There is NO undo!"))
+		  {
+
+ $.ajax({
+   type: "GET",
+   url: "deleteres.php",
+   data: info,
+   success: function(){
    
-    <div class="title-wrapper">
-        <h2>Admin Login Form</h2>
-    </div>
-    <div class="contact_form">
-        <form method="post" action="login.php" name="contact-form" id="contact-form">	
-        <div id="main">
-            <div class="one_third">
-                <label>Username:</label>
-                <p><input type="text" name="user" id="name" size="30" /></p>
-            </div>
-            <div class="one_third">
-                <label>Password:</label>
-                <p><input type="password" name="password" id="email" size="30" /></p>
-            </div>
-            <div class="one_third_last">
-			<label>&nbsp;</label>
-                <input  class="contact_button button" type="submit" name="submit" id="submit" value="Login" />
-            </div>
-        </div>
-        </form>
-    </div> 
-</li>
-<!--adminlogin End-->
-                   
-</ul>
-</article>
-</div>
-<!--Content End-->
+   }
+ });
+         $(this).parents(".record").animate({ backgroundColor: "#fbc7c7" }, "fast")
+		.animate({ opacity: "hide" }, "slow");
 
-</div>
-<!--Page wrapper End-->
+ }
+
+return false;
+
+});
+
+});
+</script>
 </body>
 </html>
